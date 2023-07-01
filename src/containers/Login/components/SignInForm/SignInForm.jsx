6228e472262fbc30df0fbae601d07664261b94d8
Yuriy Dubinin <yuradubinin100@gmail.com>
2023-07-01 20:17:00 +0300

    add

--- src/containers/Login/components/SignInForm/SignInForm.jsx
+++ src/containers/Login/components/SignInForm/SignInForm.jsx
@@ -16,6 +16,8 @@ import {login} from './actions/signInFormApi';
 
 const SignInForm = ({onChangeMode, onCloseModal}) => {
     const [passwordType, setPasswordType] = useState('password');
+    const [isFetching, setFetching] = useState(false);
+    const [isSuccess, setSuccess] = useState(null);
 
     const dispatch = useDispatch();
 
@@ -35,7 +37,7 @@ const SignInForm = ({onChangeMode, onCloseModal}) => {
     const fieldDescription = {
         email: 'Емейл должен быть написан в стандартном формате и содержать в себе символ "@"',
         password:
-            'Пароль должен содержать минимум 6 символов, одну заглавную букву, одну прописную букву, одну цифру, один специальный символ (только английский язык)',
+            'Пароль должен содержать минимум 8 символов, одну заглавную букву, одну прописную букву, одну цифру, один специальный символ (только английский язык)',
     };
 
     const onSubmit = (data) => {
@@ -59,98 +61,107 @@ const SignInForm = ({onChangeMode, onCloseModal}) => {
 
     return (
         <div className="sign-in-from">
-            <div className="sign-in-from__row">
-                <div className="sign-in-from__header">
-                    <h3>Вход</h3>
-                    <span>Введите данные для входа в систему</span>
+            {isFetching && (
+                <div className="sign-in-from__feedback">
                 </div>
-                <form className="sign-in-from__body default-form" onSubmit={handleSubmit(onSubmit)}>
-                    <div className="default-form__input-wrapper sign-in-form__email">
-                        <div
-                            className="sign-in-form__info-icon"
-                            onClick={() => {
-                                toast(fieldDescription.email, {
-                                    icon: 'ℹ️',
-                                });
-                            }}
-                        >
-                            <InfoIcon />
-                        </div>
-                        <input
-                            className="default-form__input"
-                            placeholder="Почта"
-                            {...register('email', {
-                                validate: (value) => validateEmail(value, true),
-                            })}
-                        />
-                        {errors.email && (
-                            <span className="default-form__error-message">
-                                {errors.email?.message}
-                            </span>
-                        )}
+            )}
+            {!isFetching && isSuccess === null && (
+                <div className="sign-in-from__row">
+                    <div className="sign-in-from__header">
+                        <h3>Вход</h3>
+                        <span>Введите данные для входа в систему</span>
                     </div>
-                    <div className="default-form__input-wrapper sign-in-form__password">
-                        <div
-                            className="sign-in-form__info-icon"
-                            onClick={() => {
-                                toast(fieldDescription.password, {
-                                    icon: 'ℹ️',
-                                });
-                            }}
-                        >
-                            <InfoIcon />
-                        </div>
-                        <input
-                            type={passwordType}
-                            className="default-form__input"
-                            placeholder="Пароль"
-                            {...register('password', {
-                                validate: (value) => validatePassword(value, true),
-                            })}
-                        />
-                        {errors.password && (
-                            <span className="default-form__error-message">
-                                {errors.password?.message}
-                            </span>
-                        )}
-                        {passwordType === 'password' && (
-                            <div className="sign-in-form__eye-icon">
-                                <ClosedEyeIcon onClick={() => setPasswordType('text')} />
-                            </div>
-                        )}
-                        {passwordType === 'text' && (
-                            <div className="sign-in-form__eye-icon">
-                                <OpenEyeIcon onClick={() => setPasswordType('password')} />
-                            </div>
-                        )}
-                    </div>
-                    <div className="default-form__input-wrapper sign-in-form__remember-me">
-                        <label>
-                            <input
-                                {...register('rememberMe')}
-                                type="checkbox"
-                                className="default-form__checkbox"
-                            />
-                            <span>Запомнить меня</span>
-                        </label>
-                    </div>
-                    <button
-                        type="submit"
-                        className="default-form__submit-btn sign-in-form__submit-btn"
+                    <form
+                        className="sign-in-from__body default-form"
+                        onSubmit={handleSubmit(onSubmit)}
                     >
-                        Вход
-                    </button>
-                </form>
-                <div className="sign-in-from__footer">
-                    <div className="sign-in-from__change-mode">
-                        <p onClick={() => onChangeMode('PASSWORD_RECOVERY')}>Забыли пароль?</p>
-                    </div>
-                    <div className="sign-in-from__change-mode">
-                        <span>Отсутствует аккаунт?</span>
-                        <p onClick={() => onChangeMode('REGISTRATION')}>Создать аккаунт</p>
+                        <div className="default-form__input-wrapper sign-in-form__email">
+                            <div
+                                className="sign-in-form__info-icon"
+                                onClick={() => {
+                                    toast(fieldDescription.email, {
+                                        icon: 'ℹ️',
+                                    });
+                                }}
+                            >
+                                <InfoIcon />
+                            </div>
+                            <input
+                                className="default-form__input"
+                                placeholder="Почта"
+                                {...register('email', {
+                                    validate: (value) => validateEmail(value, true),
+                                })}
+                            />
+                            {errors.email && (
+                                <span className="default-form__error-message">
+                                    {errors.email?.message}
+                                </span>
+                            )}
+                        </div>
+                        <div className="default-form__input-wrapper sign-in-form__password">
+                            <div
+                                className="sign-in-form__info-icon"
+                                onClick={() => {
+                                    toast(fieldDescription.password, {
+                                        icon: 'ℹ️',
+                                    });
+                                }}
+                            >
+                                <InfoIcon />
+                            </div>
+                            <input
+                                type={passwordType}
+                                className="default-form__input"
+                                placeholder="Пароль"
+                                {...register('password', {
+                                    validate: (value) => validatePassword(value, true),
+                                })}
+                            />
+                            {errors.password && (
+                                <span className="default-form__error-message">
+                                    {errors.password?.message}
+                                </span>
+                            )}
+                            {passwordType === 'password' && (
+                                <div className="sign-in-form__eye-icon">
+                                    <ClosedEyeIcon onClick={() => setPasswordType('text')} />
+                                </div>
+                            )}
+                            {passwordType === 'text' && (
+                                <div className="sign-in-form__eye-icon">
+                                    <OpenEyeIcon onClick={() => setPasswordType('password')} />
+                                </div>
+                            )}
+                        </div>
+                        <div className="default-form__input-wrapper sign-in-form__remember-me">
+                            <label>
+                                <input
+                                    {...register('rememberMe')}
+                                    type="checkbox"
+                                    className="default-form__checkbox"
+                                />
+                                <span>Запомнить меня</span>
+                            </label>
+                        </div>
+                        <button
+                            type="submit"
+                            className="default-form__submit-btn sign-in-form__submit-btn"
+                        >
+                            Вход
+                        </button>
+                    </form>
+                    <div className="sign-in-from__footer">
+                        <div className="sign-in-from__change-mode">
+                            <p onClick={() => onChangeMode('PASSWORD_RECOVERY')}>Забыли пароль?</p>
+                        </div>
+                        <div className="sign-in-from__change-mode">
+                            <span>Отсутствует аккаунт?</span>
+                            <p onClick={() => onChangeMode('REGISTRATION')}>Создать аккаунт</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 };
